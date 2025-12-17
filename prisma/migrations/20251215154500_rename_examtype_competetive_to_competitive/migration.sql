8645cceae536e5139f2623208b73e395dfd7be1c
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-12-17 20:02:40 +0600

    feat: removed outdated migrations

--- prisma/migrations/20251215154500_rename_examtype_competetive_to_competitive/migration.sql
+++
@@ -1,5 +0,0 @@
--- Rename enum value typo: Competetive -> Competitive
--- Safe for existing DBs where "ExamType" already exists with the misspelling.
-ALTER TYPE "ExamType" RENAME VALUE 'Competetive' TO 'Competitive';
-
-
