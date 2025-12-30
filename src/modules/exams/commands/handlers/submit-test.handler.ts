895faffdbf26c5487579987dafe4e1e5681bb670
Awesh Islam <aweshrumc@gmail.com>
2025-12-30 17:43:38 +0600

    feat(api): add new api for test evaluation;

---
+++ src/modules/exams/commands/handlers/submit-test.handler.ts
@@ -0,0 +1,13 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { SubmitTestCommand } from '../submit-test.command';
+import { ExamsService } from '../../exams.service';
+
+@CommandHandler(SubmitTestCommand)
+export class SubmitTestHandler implements ICommandHandler<SubmitTestCommand> {
+  constructor(private readonly examsService: ExamsService) {}
+
+  async execute(command: SubmitTestCommand) {
+    return this.examsService.submitTest(command.dto);
+  }
+}
+
--- src/modules/exams/exams.module.ts
+++ src/modules/exams/exams.module.ts
@@ -1,22 +1,31 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ExamsController } from './exams.controller';
+import { ExamsService } from './exams.service';
 import { CreateExamHandler } from './commands/handlers/create-exam.handler';
 import { CreateMockExamHandler } from './commands/handlers/create-mock-exam.handler';
+import { SubmitTestHandler } from './commands/handlers/submit-test.handler';
 import { GetExamHistoryHandler } from './queries/handlers/get-exam-history.handler';
 import { GetExamSolutionsHandler } from './queries/handlers/get-exam-solutions.handler';
 
 // List of all Command Handlers to be registered as providers
-export const CommandHandlers = [CreateExamHandler, CreateMockExamHandler];
+export const CommandHandlers = [
+  CreateExamHandler,
+  CreateMockExamHandler,
+  SubmitTestHandler,
+];
 
 // List of all Query Handlers to be registered as providers
 export const QueryHandlers = [GetExamSolutionsHandler, GetExamHistoryHandler];
 
 @Module({
-  imports: [CqrsModule, PrismaModule],
+  imports: [CqrsModule, PrismaModule, HttpModule, ConfigModule],
   controllers: [ExamsController],
   providers: [
+    ExamsService,
     ...CommandHandlers,
     ...QueryHandlers,
   ],
