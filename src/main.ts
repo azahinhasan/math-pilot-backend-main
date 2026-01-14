59a54b9c236ea026e1ebc3563aff4b6d558ace0d
azahinhasan <azahinhasan@gmail.com>
2026-01-14 19:59:43 +0600

    feat: increase request body size limit to 20mb using body-parser middleware

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '20mb' }));  //have to find better solution for this part
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true })); //have to find better solution for this part
+
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
